bb9d885b191ac2ac9193b45b9ea6920ebcd49b89
Pais Gomes Alexandre <m1015@ic2s130-07.univ-lemans.fr>
2014-09-30 12:01:57 +0200

    Moyenne ui

--- tp.m
+++ tp.m
@@ -1,10 +1,19 @@
 % TP - Classifieur Bayésien
 % Alexandre Pais Gomes
-%
-1;
-appr_cl = load("/info/etu/m1/m1015/Documents/aan/data/appr_cl.ascii");
 
-% Calcul des probabilités à priori p(wi)
+1;
+
+% Chargement des données
+appr_cl = load("/info/etu/m1/m1015/Documents/aan/data/appr_cl.ascii");
+appr = load("/info/etu/m1/m1015/Documents/aan/data/appr.ascii");
+
+% Calcul des probabilités à priori P(wi)
 for i = 0:9
-	pwi(i+1) = length(find(appr_cl(:) == i)) / length(appr_cl(:));
+	pwi(i+1) = mean(appr_cl(:) == i);
+end
+
+% Calcul des probabilités conditionnelles P(x|wi)
+% Moyenne ui (vecteur de dimension 256)
+for i = 0:9
+	ui(i+1,:) = mean(appr(find(appr_cl(:) == i),:));
 end
