c1b6bc2ab749ac635db6a1a1c5e5e11835e6c7f2
Pais Gomes Alexandre <m1015@ic2s130-07.univ-lemans.fr>
2014-10-02 11:05:41 +0200

    Calcul des covariances pour chaque classe

--- tp.m
+++ tp.m
@@ -7,13 +7,14 @@
 appr_cl = load("data/appr_cl.ascii");
 appr = load("data/appr.ascii");
 
-% Calcul des probabilités à priori P(wi)
 for i = 0:9
+	% Calcul des probabilités à priori P(wi)
 	pwi(i+1) = mean(appr_cl(:) == i);
-end
-
-% Calcul des probabilités conditionnelles P(x|wi)
-% Moyenne ui (vecteur de dimension 256)
-for i = 0:9
+	
+	% Calcul des probabilités conditionnelles P(x|wi)
+	% Moyenne ui (vecteur de dimension 256)
 	ui(i+1,:) = mean(appr(find(appr_cl(:) == i),:));
+	
+	% Calcul des covariances
+	covar(i+1,:,:) = cov(appr(find(appr_cl(:) == i),:));	
 end
