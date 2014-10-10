22497015c07a9960855e38ec486d8d6a9eb5267f
Pais Gomes Alexandre <m1015@ic2s130-07.univ-lemans.fr>
2014-10-10 13:49:26 +0200

    Début de la phase de classification

--- tp.m
+++ tp.m
@@ -3,22 +3,6 @@
 
 1;
 
-% Chargement des données
-%appr_cl = load("data/appr_cl.ascii");
-%appr = load("data/appr.ascii");
-%
-%for i = 0:9
-%	% Calcul des probabilités à priori P(wi)
-%	pwi(i+1) = mean(appr_cl(:) == i);
-%	
-%	% Calcul des probabilités conditionnelles P(x|wi)
-%	% Moyenne ui (vecteur de dimension 256)
-%	ui(i+1,:) = mean(appr(find(appr_cl(:) == i),:));
-%	
-%	% Calcul des covariances
-%	covar(i+1,:,:) = cov(appr(find(appr_cl(:) == i),:));	
-%end
-
 % ACP
 % Cette technique permet de réduire la dimension des données.
 % Ceci est nécessaire puisque certains points des images ont toujours la même valeur,
@@ -80,5 +64,13 @@ end
 
 % Calcul de la Gaussienne pour chaque classe
 function res = gaussienne (cl,x)
-	res = 1/(sqrt(2*pi)*covar(:,:,cl+1)^(1/10))*exp((-1/2)*(x-ui(cl+1))'*inv(covar(:,:,cl+1))*(x-ui(cl+1)));
+	res = 1/(sqrt(2*pi)*covar(:,:,cl+1)^(1/2))*exp((-1/2)*(x-ui(cl+1))'*inv(covar(:,:,cl+1))*(x-ui(cl+1)));
 endfunction
+
+% Phase de classification
+
+% Traitement séquentiel des exemples du corpus de développement
+dev_cl = load("data/dev_cl.ascii");
+dev_acp = load("data/acp/dev-acp.ascii");
+
+
